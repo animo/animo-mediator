b7b16412000f02c934e1e0c552419f9dd69383b2
Martin Auer <martin.auer97@gmail.com>
2023-03-10 19:48:45 +0100

    style: prettier formatting (#6)

--- src/storage/MessageRecord.ts
+++ src/storage/MessageRecord.ts
@@ -1,37 +1,34 @@
-import type { EncryptedMessage } from "@aries-framework/core";
+import type { EncryptedMessage } from '@aries-framework/core'
 
-import { BaseRecord, utils } from "@aries-framework/core";
+import { BaseRecord, utils } from '@aries-framework/core'
 
 export type DefaultMessageRecordTags = {
-  connectionId: string;
-};
-
-export interface MessageRecordStorageProps {
-  id?: string;
-  createdAt?: Date;
-  connectionId: string;
-  message: EncryptedMessage;
+  connectionId: string
 }
 
-export class MessageRecord
-  extends BaseRecord<DefaultMessageRecordTags>
-  implements MessageRecordStorageProps
-{
-  public sentTime!: string;
-  public connectionId!: string;
-  public message!: EncryptedMessage;
+export interface MessageRecordStorageProps {
+  id?: string
+  createdAt?: Date
+  connectionId: string
+  message: EncryptedMessage
+}
 
-  public static override readonly type = "MessageRecord";
-  public override readonly type = MessageRecord.type;
+export class MessageRecord extends BaseRecord<DefaultMessageRecordTags> implements MessageRecordStorageProps {
+  public sentTime!: string
+  public connectionId!: string
+  public message!: EncryptedMessage
+
+  public static override readonly type = 'MessageRecord'
+  public override readonly type = MessageRecord.type
 
   public constructor(props: MessageRecordStorageProps) {
-    super();
+    super()
 
     if (props) {
-      this.id = props.id ?? utils.uuid();
-      this.createdAt = props.createdAt ?? new Date();
-      this.connectionId = props.connectionId;
-      this.message = props.message;
+      this.id = props.id ?? utils.uuid()
+      this.createdAt = props.createdAt ?? new Date()
+      this.connectionId = props.connectionId
+      this.message = props.message
     }
   }
 
@@ -39,6 +36,6 @@ export class MessageRecord
     return {
       ...this._tags,
       connectionId: this.connectionId,
-    };
+    }
   }
 }
--- src/storage/StorageMessageQueueModule.ts
+++ src/storage/StorageMessageQueueModule.ts
@@ -1,11 +1,11 @@
-import type { DependencyManager, Module } from "@aries-framework/core";
+import type { DependencyManager, Module } from '@aries-framework/core'
 
-import { MessageRepository } from "./MessageRepository";
-import { StorageServiceMessageQueue } from "./StorageMessageQueue";
+import { MessageRepository } from './MessageRepository'
+import { StorageServiceMessageQueue } from './StorageMessageQueue'
 
 export class StorageMessageQueueModule implements Module {
   public register(dependencyManager: DependencyManager) {
-    dependencyManager.registerContextScoped(StorageServiceMessageQueue);
-    dependencyManager.registerSingleton(MessageRepository);
+    dependencyManager.registerContextScoped(StorageServiceMessageQueue)
+    dependencyManager.registerSingleton(MessageRepository)
   }
 }
