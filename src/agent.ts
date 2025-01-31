97d404092356be198a82296904989c4eec32a60b
al-rosenthal <alfred.k.rosenthal@gmail.com>
2025-01-31 13:29:52 -0800

    fix: updated envs
    Signed-off-by: al-rosenthal <alfred.k.rosenthal@gmail.com>

--- src/agent.ts
+++ src/agent.ts
@@ -152,7 +152,7 @@ export async function createAgent() {
         credential.cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         })
     })
   }
